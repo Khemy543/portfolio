70cbc8a93080ef10c477185256823bdb76a48b6e
Gideon Assafuah <gassafuah@gmail.com>
2024-05-17 16:42:18 -0400

    [IMP] add environment variables for email template

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,35 +1,149 @@
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import './Contact.scss';
-import SectionHeading from '../SectionHeading/SectionHeading';
-import { Icon } from '@iconify/react';
-import SocialLinks from '../SocialLinks/SocialLinks';
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import "./Contact.scss";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import { Icon } from "@iconify/react";
+import SocialLinks from "../SocialLinks/SocialLinks";
+
+import * as emailjs from "@emailjs/browser";
 
 const Contact = ({ data, socialData }) => {
   const { title, text, subTitle, formTitle, email, address } = data;
+
+  useEffect(() => {
+    emailjs.init({
+      publicKey: process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY,
+      // Do not allow headless browsers
+      blockHeadless: true,
+      blockList: {
+        // Block the suspended emails
+        list: [],
+        // The variable contains the email address
+        watchVariable: "userEmail",
+      },
+      limitRate: {
+        // Set the limit rate for the application
+        id: "app",
+        // Allow 1 request per 10s
+        throttle: 10000,
+      },
+    });
+  }, []);
+
+  const [name, setName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const templateParams = {
+      name: name,
+      email: userEmail,
+      subject: subject,
+      message: msg,
+    };
+    emailjs
+      .send(
+        process.env.REACT_APP_EMAIL_JS_SERVICE_ID,
+        process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID,
+        templateParams
+      )
+      .then(() => {
+        setAlert(true);
+        setTimeout(() => {
+          setAlert(false);
+        }, 5000);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+        setName("");
+        setUserEmail("");
+        setSubject("");
+        setMsg("");
+      });
+  };
+
   return (
     <section id="contact" className="st-dark-bg">
       <div className="st-height-b100 st-height-lg-b80"></div>
       <SectionHeading title="Contact" />
-      <div className="container" data-aos="fade-up" data-aos-duration="800" data-aos-delay="500">
+      <div
+        className="container"
+        data-aos="fade-up"
+        data-aos-duration="800"
+        data-aos-delay="500"
+      >
         <div className="row d-flex">
           <div className="col-lg-6">
             <h3 className="st-contact-title">{formTitle}</h3>
-            <div id="st-alert"></div>
-            <form action="#" method="POST" className="st-contact-form" id="contact-form">
+            {alert && <div id="st-alert">Message sent!</div>}
+            <form
+              className="st-contact-form"
+              id="contact-form"
+              onSubmit={handleSubmit}
+            >
               <div className="st-form-field">
-                <input type="text" id="name" name="name" placeholder="Your Name" required />
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Your Name"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
               <div className="st-form-field">
-                <input type="text" id="email" name="email" placeholder="Your Email" required />
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  placeholder="Your Email"
+                  required
+                  value={userEmail}
+                  onChange={(e) => setUserEmail(e.target.value)}
+                />
               </div>
               <div className="st-form-field">
-                <input type="text" id="subject" name="subject" placeholder="Your Subject" required />
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  placeholder="Your Subject"
+                  required
+                  value={subject}
+                  onChange={(e) => setSubject(e.target.value)}
+                />
               </div>
               <div className="st-form-field">
-                <textarea cols="30" rows="10" id="msg" name="msg" placeholder="Your Message" required></textarea>
+                <textarea
+                  cols="30"
+                  rows="10"
+                  id="msg"
+                  name="msg"
+                  placeholder="Your Message"
+                  required
+                  value={msg}
+                  onChange={(e) => setMsg(e.target.value)}
+                ></textarea>
               </div>
-              <button className='st-btn st-style1 st-color1' type="submit" id="submit" name="submit">Send Message</button>
+              <button
+                className="st-btn st-style1 st-color1"
+                type="submit"
+                id="submit"
+                name="submit"
+                disabled={loading}
+              >
+                Send Message
+              </button>
             </form>
             <div className="st-height-b0 st-height-lg-b30"></div>
           </div>
@@ -77,12 +191,12 @@ const Contact = ({ data, socialData }) => {
       </div>
       <div className="st-height-b100 st-height-lg-b80"></div>
     </section>
-  )
-}
+  );
+};
 
 Contact.propTypes = {
   data: PropTypes.object,
   socialData: PropTypes.array,
-}
+};
 
 export default Contact;
